43e7abcb918bd7ce8ca09105850690062554e996
SimonaBacanu <simonabacanu9@gmail.com>
2018-05-12 00:13:39 +0300

    Refactor - 4

---
+++ app/src/main/java/uiautomator/testing/android/example/com/healthcare/RecipesFragment.java
@@ -0,0 +1,18 @@
+package uiautomator.testing.android.example.com.healthcare;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class RecipesFragment extends Fragment {
+
+    private Button mButton;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+
+}
---
+++ app/src/main/java/uiautomator/testing/android/example/com/healthcare/WorkoutFragment.java
@@ -0,0 +1,19 @@
+package uiautomator.testing.android.example.com.healthcare;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class WorkoutFragment extends Fragment {
+
+    private Button mButton;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+
+
+}
